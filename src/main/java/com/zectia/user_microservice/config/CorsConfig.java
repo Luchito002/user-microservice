658e376cf858c616292c654b48ba21e6868913fa
Luisito002 <luisfernandocalleramos4@gmail.com>
2024-06-03 16:46:13 -0400

    cors config

--- src/main/java/com/zectia/user_microservice/config/CorsConfig.java
+++ src/main/java/com/zectia/user_microservice/config/CorsConfig.java
@@ -1,19 +1,32 @@
 package com.zectia.user_microservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry
-      .addMapping("/**")
-      .allowedOrigins("*")
-      .allowedMethods("*");
-  }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // Permitir solicitudes desde cualquier origen
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+
+        // Definir otros encabezados permitidos
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept"));
+
+        // Definir métodos permitidos
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
 }
