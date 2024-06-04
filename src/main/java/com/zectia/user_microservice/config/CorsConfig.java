27a5087c8df676605ef3f038bb11c22e3b271c63
Luisito002 <luisfernandocalleramos4@gmail.com>
2024-06-04 18:26:18 -0400

    cors configuration

--- src/main/java/com/zectia/user_microservice/config/CorsConfig.java
+++ src/main/java/com/zectia/user_microservice/config/CorsConfig.java
@@ -7,26 +7,36 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        // Permitir solicitudes desde cualquier origen
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+    // Permitir solicitudes desde cualquier origen
+    corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 
-        // Definir otros encabezados permitidos
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept"));
+    // Definir otros encabezados permitidos
+    corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept"));
 
-        // Definir métodos permitidos
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    // Definir métodos permitidos
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
 
-        return new CorsFilter(source);
-    }
+    return new CorsFilter(source);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*");
+  }
 }
