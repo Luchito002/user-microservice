91d2658c9eaeca6f1bf0092db1f0edf14700878c
Luisito002 <luisfernandocalleramos4@gmail.com>
2024-06-05 17:29:11 -0400

    add config cors origin

--- src/main/java/com/zectia/user_microservice/config/SecurityConfig.java
+++ src/main/java/com/zectia/user_microservice/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("*")); // Permite todos los encabezados
     configuration.setAllowCredentials(true); // Habilita las credenciales en las solicitudes CORS
